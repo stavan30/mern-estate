b0130247890d0f710cf6f1274366c56219afdc9c
stavan30 <stavanpatel107@gmail.com>
2024-01-17 01:30:49 -0800

    Backend_run and start server

---
+++ .history/client/src/components/Header_20240117003823.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Skyline</span>
+            <span className='text-slate-700'>Estate</span>
+        </h1>
+
+        <form>
+            <input type='text' placeholder='Search...'/>
+        </form>
+        </div>
+    </header>
+  )
+}
---
+++ .history/client/src/components/Header_20240117005443.jsx
@@ -0,0 +1,20 @@
+import { FaSearch} from 'react-icons/fa'
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Skyline</span>
+            <span className='text-slate-700'>Estate</span>
+        </h1>
+
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <input type='text' placeholder='Search...'
+                className='bg-transparent focus:outline-none w-24 sm:w-64'
+            />
+            <FaSearch className='text-slate-600'/>
+        </form>
+        </div>
+    </header>
+  )
+}
