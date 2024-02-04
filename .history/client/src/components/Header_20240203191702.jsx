8e42167a9a9168babdd5a37cd8d84a46c7f3ffc2
stavan30 <stavanpatel107@gmail.com>
2024-02-03 19:29:53 -0800

    update the header n make the profile page private

--- .history/client/src/components/Header_20240203191702.jsx
+++ .history/client/src/components/Header_20240203191702.jsx
@@ -24,7 +24,7 @@ export default function Header() {
         <ul className='flex gap-4'>
             <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
             <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-            <Link to='/profile'>{currentUser ? (<img className='rounded-full w-7 h-7 object-cover' src={currentUser.avatar} alt='profile'/>) : (<li className=' text-slate-700 hover:underline'>Sign in</li>)}</Link>
+            <Link to='/sign-in'>{currentUser ? (<img className='rounded-full w-7 h-7 object-cover' src={currentUser.avatar} alt='profile'/>) : (<li className=' text-slate-700 hover:underline'>Sign in</li>)}</Link>
         </ul>
         </div>
     </header>
